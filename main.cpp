0e97dc2a6bc5eb8a16934e60b4f7e5a4b1956eb4
Sorshi <damian.fuchler@gmail.com>
2022-06-23 01:01:24 +0200

    Add argument error

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@
 void createFile(const std::string name, const int count, const char value) {
     std::fstream f;
     f.open(name.c_str(), std::ios::binary | std::ios::out);
-    for (int i = 0; i < count; i++)
-    {
+    for (int i = 0; i < count; i++) {
         f.write((char*)&value,1);
     }
     f.close();
@@ -16,8 +15,7 @@ std::fstream logFile;
 
 void openLog(std::string fileName) {
     logFile.open(fileName.c_str(), std::ios_base::app);
-    if (!logFile.good() == true)
-    {
+    if (!logFile.good() == true) {
         std::cout << "Cannot open log file " << "log.txt" << std::endl;
     }
 }
@@ -43,7 +41,10 @@ int main(int argc, char** argv) {
     // createFile("file1.bin", 100, 0xFF); - Create file 1
     // createFile("file2.bin", 100, 0xFE); - Create file 2
 
+    openLog("log.txt");
+    if (argc != 3) {
+        saveLog("Too many arguments");
+    }
 
-  
     return 0;
 }

9dc36033274f4d845cee7c75cf4df88a329b1879
Sorshi <damian.fuchler@gmail.com>
2022-06-23 00:45:58 +0200

    Add function to save log file

--- main.cpp
+++ main.cpp
@@ -23,6 +23,19 @@ void openLog(std::string fileName)
     }
 }
 
+void saveLog(std::string msg)
+{
+    std::string ss;
+    time_t currentTime = time(NULL);
+    std::string txttime = (std::string)asctime(localtime(&currentTime));
+    txttime = txttime.substr(0, txttime.length() - 1);
+    ss = (std::string)"T: " + txttime + " M: " + msg + "\n";
+    logFile << ss.c_str();
+    logFile.flush();
+    std::cout << ss.c_str();
+    std::cout.flush();
+}
+
 int main(int argc, char** argv) {
 
     // createFile("file1.bin", 100, 0xFF); - Create file 1

1c5d1bb4eb48cd73772f8e6e36a6d65c06401a72
Sorshi <damian.fuchler@gmail.com>
2022-06-23 00:45:10 +0200

    Add function for open log file

--- main.cpp
+++ main.cpp
@@ -12,9 +12,23 @@ void createFile(const std::string name, const int count, const char value) {
     f.close();
 }
 
+std::fstream logFile;
+
+void openLog(std::string fileName)
+{
+    logFile.open(fileName.c_str(), std::ios_base::app);
+    if (!logFile.good() == true)
+    {
+        std::cout << "Cannot open log file " << "log.txt" << std::endl;
+    }
+}
+
 int main(int argc, char** argv) {
-    createFile("file1.bin", 100, 0xFF);
-    createFile("file2.bin", 100, 0xFE);
+
+    // createFile("file1.bin", 100, 0xFF); - Create file 1
+    // createFile("file2.bin", 100, 0xFE); - Create file 2
+
+
   
     return 0;
 }
